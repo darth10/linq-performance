d02cc44077d7cd61d61ec338c23b5db5e2c17674
Akhil Wali <akhil.wali.10@gmail.com>
2018-05-10 18:34:25 +1200

    Added benchmarks

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using BenchmarkDotNet.Running;
 
 namespace linq_perf
 {
@@ -6,7 +7,8 @@ namespace linq_perf
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // BenchmarkRunner.Run<WhereSelectBenchmarks>();
+            BenchmarkRunner.Run<FirstBenchmarks>();
         }
     }
 }
