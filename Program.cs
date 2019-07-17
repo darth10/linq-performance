74290c4d311c7f2f5fe9c2f34539cd4d330cba8f
Akhil Wali <akhil.wali.10@gmail.com>
2019-07-17 22:06:37 +1200

    Removed transducer implementation

--- Program.cs
+++ Program.cs
@@ -9,20 +9,8 @@ namespace linq_perf
     {
         static void Main(string[] args)
         {
-            // BenchmarkRunner.Run<WhereSelectBenchmarks>();
+            BenchmarkRunner.Run<WhereSelectBenchmarks>();
             // BenchmarkRunner.Run<FirstBenchmarks>();
-            // BenchmarkRunner.Run<TransducerBenchmarks>();
-
-//            List<int> itemsList = Enumerable.Range(0, 70).ToList();
-            List<int> results =
-                Enumerable.Range(0, 70).Transduce(
-                    new FilterTransducer<int>(i => i % 10 == 0).Transduce(
-                        new MapTransducer<int, int>(i => i + 5).Transduce(
-                            new ToListReducer<int>())));
-
-            Console.WriteLine("Results: " + string.Join(',', results));
-
-            // Output: Results: 5,15,25,35,45,55,65
         }
     }
 }
